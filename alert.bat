cc085f681227f51614d0954c322af4812f70e1fc
KKL999 <151863569+KKL999@users.noreply.github.com>
2023-11-24 05:21:51 +0800

    Add files via upload

---
+++ alert.bat
@@ -0,0 +1,3 @@
+d:
+cd C:\Users\Administrator\Desktop\alertmanager-0.26.0.windows-amd64
+alertmanager --config.file=alertmanager.yml
---
+++ mysql.bat
@@ -0,0 +1,3 @@
+d:
+cd C:\Users\Administrator\Desktop\mysqld_exporter-0.15.0.windows-amd64
+mysqld_exporter.exe --config.my-cnf=my.cnf
